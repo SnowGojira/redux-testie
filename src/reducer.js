d586a78d3b81bb0515c782f96c16f5cdd9b6009c
haku <hakuhal.snow@gmail.com>
2020-11-10 20:27:01 +0800

    init

---
+++ src/reducer.js
@@ -0,0 +1,18 @@
+//bugs=[...]
+//action = {type:"...",payload:{...}}
+
+let lastId = 0;
+function reducer(state=[], action){
+    if(action.type === 'bugAdded')
+    return [...state,//会有深拷贝的问题，使用immer啥的
+    {   
+        id:++lastId,
+        description:action.payload.descripton,
+        resolved:false
+    }];
+    else if(action.type==='bugRemoved')
+    return state.filter(bug=>bug.id!==action.payload.id);
+
+    return state;
+
+}
