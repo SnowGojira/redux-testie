22b8e2ec7d5d90a7543c0333660d933ac8197ff8
haku <hakuhal.snow@gmail.com>
2020-11-10 22:55:15 +0800

    完成actions Type的集中

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import store from "./store";
+import * as actions from "./actionType";
 
 const unsubscribe = store.subscribe(()=>{
     console.log("changed!!");
@@ -6,7 +7,7 @@ const unsubscribe = store.subscribe(()=>{
 })
 
 store.dispatch({
-    type:"bugAdded",
+    type:actions.BUG_ADDED,
     payload:{
         description:"bug1"
     }
@@ -15,7 +16,7 @@ store.dispatch({
 unsubscribe();
 
 store.dispatch({
-    type:"bugRemoved",
+    type:actions.BUG_REMOVED,
     payload:{
         id:1
     }
--- src/reducer.js
+++ src/reducer.js
@@ -1,17 +1,16 @@
-//bugs=[...]
-//action = {type:"...",payload:{...}}
+import * as actions from "./actionType";
 
 let lastId = 0;
 
 export default function reducer(state=[], action){
-    if(action.type === 'bugAdded')
+    if(action.type === actions.BUG_ADDED)
     return [...state,//会有深拷贝的问题，使用immer啥的
     {   
         id:++lastId,
         description:action.payload.description,
         resolved:false
     }];
-    else if(action.type==='bugRemoved')
+    else if(action.type=== actions.BUG_REMOVED)
     return state.filter(bug=>bug.id!==action.payload.id);
 
     return state;
