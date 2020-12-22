555a44132b7f6fcde17bd70b12dbf9a167afb875
haku <hakuhal.snow@gmail.com>
2020-12-22 13:53:46 +0800

    创建基础的reducer

--- src/reducer.js
+++ src/reducer.js
@@ -1,25 +1,28 @@
 import * as actions from "./actionType";
 
 let lastId = 0;
+const defaultState = [];
 
-export default function reducer(state=[], action){
-    if(action.type === actions.BUG_ADDED)
-    return [...state,//会有深拷贝的问题，使用immer啥的
+export default function reducer(state=defaultState, action){
+    switch(action.type){
+    //如果添加bug的动作发生，那么在state里面添加一条日志
+    case actions.BUG_ADDED:
+    
+    return [...state,
     {   
         id:++lastId,
         description:action.payload.description,
         resolved:false
     }];
-    else if(action.type=== actions.BUG_REMOVED)
+    //如果移除bug的动作发生，那么筛掉移除bug的那条日志
+    case actions.BUG_REMOVED:
     return state.filter(bug=>bug.id!==action.payload.id);
-
-    else if(action.type === actions.BUG_RESOLVED){
+    //如果解决bug的动作放生，那么把bug的状态标记为“已解决”
+    case actions.BUG_RESOLVED:
         return state.map(bug=>
             bug.id!==action.payload.id?
                      bug:{...bug,resolved:true});
-    }
-
-
-    return state;
-
+    //不做任何动作返回state
+    default: state;
+ }
 }
